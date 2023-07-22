e50b3bcf3bc80f3a6a7f3007d699571e26674413
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-22 11:51:37 +0600

    Create Visualize_Goldbach_Combinations_3.py

---
+++ Visualize_Goldbach_Combinations_3.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(np.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def primes_up_to_limit(limit):
+    return [num for num in range(2, limit + 1) if is_prime(num)]
+
+def goldbach_combinations(limit):
+    even_numbers = [num for num in range(4, limit + 1, 2)]
+    primes = primes_up_to_limit(limit)
+    
+    combinations = {num: [] for num in even_numbers}
+    
+    for even_num in even_numbers:
+        for prime in primes:
+            if prime >= even_num:
+                break
+            complement = even_num - prime
+            if is_prime(complement):
+                combinations[even_num].append((prime, complement))
+    
+    return combinations
+
+def visualize_goldbach_combinations(combinations):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for even_num, primes in combinations.items():
+        num_combinations = len(primes)
+
+        for prime_pair in primes:
+            x = 0
+            y = prime_pair[0]
+            z = prime_pair[1]
+            dx = num_combinations * 0.1
+            dy = 0
+            dz = 0
+
+            # Plot 3D arrow representing the prime pair
+            ax.quiver(x, y, z, dx, dy, dz, color=plt.cm.jet(num_combinations/len(combinations)), arrow_length_ratio=0.2)
+
+    ax.set_xlabel('Even Numbers')
+    ax.set_ylabel('First Prime')
+    ax.set_zlabel('Second Prime')
+    ax.set_title("Goldbach's Conjecture 3D Visualization")
+
+    plt.show()
+
+def print_goldbach_combinations(combinations):
+    for even_num, primes in combinations.items():
+        print(f"{even_num} = " + " + ".join([f"{x[0]} + {x[1]}" for x in primes]))
+
+# Let's verify Goldbach's Conjecture for even numbers up to a certain limit
+limit = 100
+combinations = goldbach_combinations(limit)
+
+# Print the combinations for each even number
+print_goldbach_combinations(combinations)
+
+# Visualize the combinations in 3D plot
+visualize_goldbach_combinations(combinations)
