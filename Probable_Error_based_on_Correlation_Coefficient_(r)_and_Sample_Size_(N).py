e295f723ddcdb8adfa526b94d3565d00db102d6d
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-23 11:21:55 +0600

    Create Probable_Error_based_on_Correlation_Coefficient_(r)_and_Sample_Size_(N).py

---
+++ Probable_Error_based_on_Correlation_Coefficient_(r)_and_Sample_Size_(N).py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Function to calculate the probable error for 'r'
+def probable_error(r, N):
+    return 0.674 * (1 - r**2) / np.sqrt(N)
+
+# Given correlation coefficient (r) and sample size (N)
+r = 0.8
+N = 25
+
+# Calculate the probable error
+prob_error = probable_error(r, N)
+print(f"Probable Error: {prob_error:.4f}")
+
+# Generate data points for correlation coefficient (r) and sample size (N)
+r_values = np.linspace(-1, 1, 100)  # 'r' range from -1 to 1
+N_values = np.arange(10, 201, 10)   # Sample sizes from 10 to 200 with step 10
+
+# Create a meshgrid from the data points
+R, N = np.meshgrid(r_values, N_values)
+
+# Calculate the probable error for each combination of 'r' and 'N'
+pe_values = probable_error(R, N)
+
+# 3D Visualization using matplotlib
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(R, N, pe_values, cmap='cool')
+
+# Customize the plot
+ax.set_xlabel('Correlation Coefficient (r)')
+ax.set_ylabel('Sample Size (N)')
+ax.set_zlabel('Probable Error (P.E.)')
+ax.set_title('Probable Error based on Correlation Coefficient (r) and Sample Size (N)')
+
+# Show the plot
+plt.show()
