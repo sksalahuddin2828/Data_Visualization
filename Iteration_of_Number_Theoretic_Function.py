c61820064a38f48fb250f33d9c82a2c4a3b9d6c4
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-24 16:23:52 +0600

    Create Iteration_of_Number_Theoretic_Function.py

---
+++ Iteration_of_Number_Theoretic_Function.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Memoization dictionary to store function results
+memo = {}
+
+def f(n):
+    if n in memo:
+        return memo[n]
+
+    initial_n = n
+    while n % 2 != 0:
+        n = 3 * n + 2
+
+    result = n // 2
+    memo[initial_n] = result
+    return result
+
+# Generate data points for visualization
+num_points = 1000
+n_values = np.arange(num_points)
+x_values, y_values, z_values = [], [], []
+
+for n in n_values:
+    x = n
+    y = f(n)
+    z = n % 2  # Color points differently based on parity (even or odd)
+    x_values.append(x)
+    y_values.append(y)
+    z_values.append(z)
+
+# Create a 3D scatter plot for visualization
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Adjust marker size and color map
+marker_size = 10
+cmap = plt.cm.get_cmap('viridis', 2)  # Use 2 colors based on parity (even or odd)
+
+# Scatter plot
+ax.scatter(x_values, y_values, z_values, c=z_values, cmap=cmap, s=marker_size)
+
+# Labels and title
+ax.set_xlabel('n')
+ax.set_ylabel('f(n)')
+ax.set_zlabel('Parity (0: even, 1: odd)')
+ax.set_title('Iteration of Number Theoretic Function f(2n)=n, f(2n+1)=f(3n+2)')
+
+# Show the plot
+plt.show()
