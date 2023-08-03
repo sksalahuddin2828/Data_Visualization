8736de24156c5110de027f2805a42c0add910597
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-08-03 13:47:57 +0600

    Create 3D_venn_diagram_plotly.py

---
+++ 3D_venn_diagram_plotly.py
@@ -0,0 +1,43 @@
+import plotly.graph_objects as go
+
+# Create a Venn diagram for a + b = b + a
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=[0.5], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='a + b'))
+fig.add_trace(go.Scatter(x=[0.6], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='b + a'))
+fig.update_layout(title='Commutative Property: a + b = b + a', showlegend=True)
+fig.show()
+
+# Create a Venn diagram for a * b = b * a
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=[0.5], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='a * b'))
+fig.add_trace(go.Scatter(x=[0.6], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='b * a'))
+fig.update_layout(title='Commutative Property: a * b = b * a', showlegend=True)
+fig.show()
+
+# Create a Venn diagram for a + 0 = a
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=[0.5], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='a + 0'))
+fig.add_trace(go.Scatter(x=[0.6], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='a'))
+fig.update_layout(title='Identity Property: a + 0 = a', showlegend=True)
+fig.show()
+
+# Create a Venn diagram for a * 1 = a
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=[0.5], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='a * 1'))
+fig.add_trace(go.Scatter(x=[0.6], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='a'))
+fig.update_layout(title='Identity Property: a * 1 = a', showlegend=True)
+fig.show()
+
+# Create a Venn diagram for a + a' = 1
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=[0.5], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='a + a\''))
+fig.add_trace(go.Scatter(x=[0.6], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='1'))
+fig.update_layout(title='Complemented Law: a + a\' = 1', showlegend=True)
+fig.show()
+
+# Create a Venn diagram for a * a' = 0
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=[0.5], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='a * a\''))
+fig.add_trace(go.Scatter(x=[0.6], y=[0.5], mode='markers', marker=dict(size=400, color='rgba(0, 0, 0, 0)', line=dict(color='black', width=2)), name='0'))
+fig.update_layout(title='Complemented Law: a * a\' = 0', showlegend=True)
+fig.show()
