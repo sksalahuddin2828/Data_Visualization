ddacaa6591043c990bd505f55b8733208cb9a280
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-08-04 17:12:10 +0600

    Create Creative_3D_Visualization_of_5D_Hypercube.py

---
+++ Creative_3D_Visualization_of_5D_Hypercube.py
@@ -0,0 +1,48 @@
+import numpy as np
+import plotly.graph_objects as go
+
+def generate_hypercube_vertices():
+    vertices = []
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                for l in range(2):
+                    for m in range(2):
+                        vertices.append([i, j, k, l, m])
+    return vertices
+
+def generate_hypercube_edges(vertices):
+    edges = []
+    for i in range(len(vertices)):
+        for j in range(i+1, len(vertices)):
+            dist = np.sum(np.array(vertices[i]) != np.array(vertices[j]))
+            if dist == 1:
+                edges.append([vertices[i], vertices[j]])
+    return edges
+
+def project_to_3d(vertices):
+    return [vertex[:3] for vertex in vertices]
+
+def visualize_hypercube(vertices, edges):
+    fig = go.Figure()
+
+    for edge in edges:
+        points = np.array(edge)
+        x, y, z = points[:, 0], points[:, 1], points[:, 2]
+        fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='lines', line=dict(color='blue'), name='Hypercube Edge'))
+
+    fig.update_layout(scene=dict(xaxis_title='X', yaxis_title='Y', zaxis_title='Z'),
+                      title='Creative 3D Visualization of 5D Hypercube',
+                      showlegend=True,
+                      width=600, height=600)  # Set width and height here
+
+    fig.show()
+
+def main():
+    vertices = generate_hypercube_vertices()
+    edges = generate_hypercube_edges(vertices)
+    vertices_3d = project_to_3d(vertices)
+    visualize_hypercube(vertices_3d, edges)
+
+if __name__ == "__main__":
+    main()
