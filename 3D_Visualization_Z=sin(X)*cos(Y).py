6dc75b033ada9bb08382361513806f535f924b0f
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-08-15 11:56:31 +0600

    Create 3D_Visualization_Z=sin(X)*cos(Y).py

---
+++ 3D_Visualization_Z=sin(X)*cos(Y).py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Generate data
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+X, Y = np.meshgrid(x, y)
+Z = np.sin(X) * np.cos(Y)
+
+# Create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the surface
+surf = ax.plot_surface(X, Y, Z, cmap='viridis')
+
+# Add color bar
+fig.colorbar(surf, ax=ax)
+
+# Set labels
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Add a title
+ax.set_title('Creative 3D Visualization')
+
+# Show the plot
+plt.show()
