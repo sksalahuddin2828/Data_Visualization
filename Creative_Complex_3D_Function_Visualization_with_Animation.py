97d725c72c96f665fbd730e5b3a9f89bab3861c5
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-08-20 12:47:56 +0600

    Create Creative_Complex_3D_Function_Visualization_with_Animation.py

---
+++ Creative_Complex_3D_Function_Visualization_with_Animation.py
@@ -0,0 +1,47 @@
+import numpy as np
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+
+# Define the function to be visualized
+def complex_function(x_val, a_val):
+    return np.exp(-a_val) * np.sin(x_val) * np.sqrt(a_val**2 + x_val**2)
+
+# Create x and a values
+x_vals = np.linspace(0, 5, 100)
+a_vals = np.linspace(0.1, 5, 50)
+
+# Create meshgrid
+X, A = np.meshgrid(x_vals, a_vals)
+Z = complex_function(X, A)
+
+# Create a DataFrame
+df = pd.DataFrame({'x': X.flatten(), 'a': A.flatten(), 'value': Z.flatten()})
+
+# Create an interactive 3D surface plot with dynamic color transitions and animation
+fig = px.scatter_3d(df, x='x', y='a', z='value', opacity=0.7,
+                    title='Creative Complex 3D Function Visualization with Animation',
+                    labels={'value': 'Function Value'},
+                    color='value',
+                    color_continuous_scale=px.colors.sequential.Viridis,
+                    animation_frame='a')  # Animation frame parameter
+
+# Add rotating animation effect by setting frame animation duration
+rotation_frames = []
+for angle in np.linspace(0, 360, len(a_vals)):
+    rotation_frames.append(go.Frame(layout_scene=dict(camera_eye=dict(x=1.25*np.cos(np.radians(angle)),
+                                                                      y=1.25*np.sin(np.radians(angle)),
+                                                                      z=0.5),
+                                                     xaxis_title='x', yaxis_title='a', zaxis_title='Function Value')))
+fig.frames = rotation_frames
+
+# Create play and pause buttons for animation
+fig.update_layout(updatemenus=[dict(type='buttons', showactive=False, buttons=[dict(label='Play',
+                                method='animate', args=[None, dict(frame=dict(duration=100, redraw=True), fromcurrent=True, mode='immediate')]),
+                               dict(label='Pause', method='animate', args=[[None], dict(frame=dict(duration=0, redraw=False), mode='immediate')])])])
+
+# Create slider for animation speed control
+fig.update_layout(sliders=[dict(currentvalue=dict(prefix='Speed: ', visible=True),
+                                steps=[dict(label=str(duration), method='animate', args=[[None], dict(frame=dict(duration=duration, redraw=True), mode='immediate')]) for duration in [100, 50, 10, 1]])])
+
+fig.show()
