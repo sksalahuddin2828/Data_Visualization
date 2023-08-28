ee6149101e5c899220598f93ee0d3d31cfed4f3f
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-08-28 13:57:51 +0600

    Create 3D_Peak_Current_of_a_Room_Heater_(Voltage_and_Power).py

---
+++ 3D_Peak_Current_of_a_Room_Heater_(Voltage_and_Power).py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Given parameters
+power = 500  # in watts
+voltage = 120  # in volts
+
+# Calculate resistance using R = V^2 / P
+resistance = voltage**2 / power
+
+# Calculate peak current using Ohm's law: I = V / R
+peak_current = voltage / resistance
+
+# Create a range of voltage and power values for visualization
+voltage_range = np.linspace(100, 150, 20)
+power_range = np.linspace(300, 700, 20)
+voltage_grid, power_grid = np.meshgrid(voltage_range, power_range)
+current_grid = voltage_grid / (voltage_grid**2 / power_grid)
+
+# Create a Pandas DataFrame to store the data
+data = {'Voltage': voltage_grid.flatten(),
+        'Power': power_grid.flatten(),
+        'Current': current_grid.flatten()}
+df = pd.DataFrame(data)
+
+# Create an interactive 3D plot using Plotly
+fig = px.scatter_3d(df, x='Voltage', y='Power', z='Current',
+                    title='Peak Current of a Room Heater',
+                    labels={'Voltage': 'Voltage (V)', 'Power': 'Power (W)', 'Current': 'Current (A)'},
+                    opacity=0.7)
+fig.update_traces(marker=dict(size=5), selector=dict(mode='markers'))
+fig.show()
+
+# Create a 3D plot using Matplotlib
+fig_matplotlib = plt.figure(figsize=(10, 6))
+ax = fig_matplotlib.add_subplot(111, projection='3d')
+ax.plot_surface(voltage_grid, power_grid, current_grid, cmap='viridis')
+ax.set_xlabel('Voltage (V)')
+ax.set_ylabel('Power (W)')
+ax.set_zlabel('Current (A)')
+ax.set_title('Peak Current of a Room Heater')
+plt.show()
